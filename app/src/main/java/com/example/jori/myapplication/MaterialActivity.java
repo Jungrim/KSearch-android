e6ca9e95108412315b39711a9b787b2b38ada8cd
Jori <sjr0930@gmail.com>
2015-01-15 02:25:40 +0900

    첫 commit 입니다!

---
+++ app/src/main/java/com/example/jori/myapplication/MaterialActivity.java
@@ -0,0 +1,47 @@
+package com.example.jori.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MaterialActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material);
+
+        Intent intent = getIntent();
+        String intent_name = intent.getStringExtra("intent_name");
+        TextView tv = (TextView)findViewById(R.id.material_text);
+        tv.setText(intent_name);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_material, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
