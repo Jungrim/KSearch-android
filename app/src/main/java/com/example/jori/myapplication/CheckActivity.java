c2d1dd9f949715003af0aa35f4935ec94344430a
KylePark <lovegjgjgj@naver.com>
2015-01-24 01:40:56 +0900

    검사기관 쿼리문 수정완료및 CustomBaseAdapter와 연동완료

--- app/src/main/java/com/example/jori/myapplication/CheckActivity.java
+++ app/src/main/java/com/example/jori/myapplication/CheckActivity.java
@@ -2,22 +2,17 @@ package com.example.jori.myapplication;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
 import android.app.ProgressDialog;
 import android.view.KeyEvent;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import org.json.JSONObject;
 import org.json.JSONArray;
@@ -39,21 +34,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.os.AsyncTask;
-import android.content.Intent;
 import android.widget.Toast;
+
+import javax.xml.transform.Result;
 
 
 public class CheckActivity extends ActionBarActivity {
     //BigMiddleConnect[] partList;
-    String[] bigList;
-    Spinner bigSpinner;
-    Spinner middleSpinner;
-    Button searchButton;
-    BigMiddleConnect[] dataList;
-    String selectBigname;
-    String selectMiddlename;
-    ListView resultView;
-    Vector<resultData> results;
+    private String[] bigList;
+    private Spinner bigSpinner;
+    private Spinner middleSpinner;
+    private Button searchButton;
+    private BigMiddleConnect[] dataList;
+    private String selectBigid;
+    private String selectMiddleid;
+    private ListView resultView;
+    private ArrayList<ResultData> results;
 //    resultData[] results;
 
     @Override
@@ -83,7 +79,7 @@ public class CheckActivity extends ActionBarActivity {
         }
 
         public void onItemSelected(AdapterView<?> parent, View view, int position, long Id){
-            selectMiddlename = new String(dataList[this.id].getMiddleName(position));
+            selectMiddleid = new String(dataList[this.id].getMiddleId(position));
         }
 
         public void onNothingSelected(AdapterView<?> parent) {
@@ -92,41 +88,53 @@ public class CheckActivity extends ActionBarActivity {
     }
 
     private class ResultTask extends AsyncTask<Void, Void, Void>{
-        ArrayAdapter resultAdapter;
+        //검색 버튼 누를시에 작동하는 AsyncTask로 resultData를 만든다.
+//        ArrayAdapter<String> resultAdapter;
+        private ArrayList<InfoClass> infoList;
+        CustomBaseAdapter resultAdapter;
         private ProgressDialog dialog;
 
         @Override
         protected void onPreExecute() {
+            //쿼리를 날려 데이터를 만들기 전에 미리 작동되는 부분으로
+            //로딩 창을 만들어서 띄워줌
+            infoList = new ArrayList<InfoClass>();
             dialog = new ProgressDialog(CheckActivity.this);
   //          dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
             dialog.setMessage("Loading");
             dialog.show();
-
             super.onPreExecute();
         }
 
         protected Void doInBackground(Void... voids){
+            //쿼리를 날려서 데이터를 얻어오고 어레이 어댑터를 만듬
             makeSearchQuery();
-            resultAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1);
+
+//            resultAdapter = new CustomBaseAdapter(getApplicationContext(),(InfoClass)results);
+//            resultAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1);
             return null;
         }
         protected void onPostExecute(Void id) {
+            //데이터를 모두 얻어온 다음 실행되는 부분으로 로딩창을 끝내고
+            //얻어온 데이터를 뷰에 붙여서 보여줌
+            //후에 정림이가 만든 ListView로 수정
+
             dialog.dismiss();
-            for(int i=0;i<results.size();i++){
-                resultAdapter.add(results.get(i).getData());
+            for(ResultData tmpData : results){
+                infoList.add(new InfoClass(tmpData.getData(),getResources().getDrawable(R.drawable.ic_launcher),""+ (results.indexOf(tmpData)+1)));
             }
-            resultView.setAdapter(resultAdapter);
-//            for(int i=0;i<results.length;i++){
-//                resultAdapter.add(results[i].getData());
-//            }
+            resultView.setAdapter(new CustomBaseAdapter(getApplicationContext(),infoList));
+
         }
     }
 
     private class MiddleAdapterTask extends AsyncTask<Integer, Void, Integer>{
+        //대분류가 선택되었을때 대분류에 해당하는 세부분야를 불러오는 AsyncTask
         private ProgressDialog dialog;
-
         @Override
         protected void onPreExecute() {
+            //쿼리를 만들어서 데이터를 불러오기전에 실행되는 부분
+            //로딩창을 만들어서 띄워줌
             dialog = new ProgressDialog(CheckActivity.this);
             //          dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
             dialog.setMessage("Loading");
@@ -136,10 +144,13 @@ public class CheckActivity extends ActionBarActivity {
         }
 
         protected Integer doInBackground(Integer... ids){
+            //입력받은 대분류Id(bigid)를 통해서 해당되는 세부분야(middlename)를 불러옴
             dataList[ids[0]].setMiddleList();
             return ids[0];
         }
         protected void onPostExecute(Integer id){
+            //불러와진 데이터를 통해 어레이어댑터를 만들고
+            //미들스피너에 붙이고 로딩창 종료
             ArrayAdapter middleAdapter = new myAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, dataList[id].getMiddleList());
             middleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
             middleSpinner.setAdapter(middleAdapter);
@@ -149,20 +160,22 @@ public class CheckActivity extends ActionBarActivity {
     }
 
     private class BigAdapterTask extends AsyncTask<String, Void, String> {
+        //최초에 체크액티비티가 켜졌을때 대분류를 불러오는 AsyncTask
         ArrayAdapter bigAdapter;
         private ProgressDialog dialog;
 
         @Override
         protected void onPreExecute() {
+            //쿼리문을 만들어 데이터를 불러오기전에 실행되는 부분
+            //로딩창을 만들어서 듸워줌
             dialog = new ProgressDialog(CheckActivity.this);
-            //          dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
             dialog.setMessage("Loading");
             dialog.show();
-
             super.onPreExecute();
         }
         @Override
         protected String doInBackground(String... str) {
+            //쿼리를 만들어서 대분류를 불러오고, 어레이리스트인 bigAdapter를 만든다.
             makeDataList();
             bigAdapter = setBigAdapter();
 
@@ -172,6 +185,7 @@ public class CheckActivity extends ActionBarActivity {
         @Override
         protected void onPostExecute(String result) {
             //makeDataList에서 만들어진 bigList를 apdater를 통해 spinner에 입력
+            //로딩창을 끝냄.
             bigSpinner.setAdapter(bigAdapter);
             dialog.dismiss();
             bigSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
@@ -180,7 +194,7 @@ public class CheckActivity extends ActionBarActivity {
                     for(int i=0;i<dataList.length;i++){
                         if(bigList[position].equals(dataList[i].getBigname())) {
                             new MiddleAdapterTask().execute(i);
-                            selectBigname = new String(bigList[position]);
+                            selectBigid = new String(dataList[i].getBigId());
                         }
                     }
                 }
@@ -195,6 +209,8 @@ public class CheckActivity extends ActionBarActivity {
     }
 
     private class myAdapter extends ArrayAdapter<String> {
+        //스피너에 붙는 어레이 어댑터를 만들때 사용되는 클래스로
+        //어레이어댑터를 상속하고 TextView를 통해서 폰트의 설정을 변경할 수 있다.
         Context context;
         String[] items = new String[] {};
 
@@ -204,11 +220,8 @@ public class CheckActivity extends ActionBarActivity {
             this.items = objects;
             this.context = context;
         }
-
-        /**
-         * 스피너 클릭시 보여지는 View의 정의
-         */
         @Override
+        //스피너 클릭시 보여지는 View의 정의
         public View getDropDownView(int position, View convertView,
                                     ViewGroup parent) {
 
@@ -221,15 +234,13 @@ public class CheckActivity extends ActionBarActivity {
             TextView tv = (TextView) convertView.findViewById(android.R.id.text1);
             tv.setText(items[position]);
             tv.setTextColor(Color.BLACK);
-            tv.setTextSize(12);
+            tv.setTextSize(15);
             tv.setHeight(50);
             return convertView;
         }
 
-        /**
-         * 기본 스피너 View 정의
-         */
         @Override
+        //기본 스피터 View의 정의
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
                 LayoutInflater inflater = LayoutInflater.from(context);
@@ -251,20 +262,27 @@ public class CheckActivity extends ActionBarActivity {
     }
 
     private class BigMiddleConnect{
-        int bigId;
-        String bigname;
-        String[] middleList;
-        String middleStr = "";
+        //대분류와 그에 해당하는 세부분야들을 갖고 있으며 get메소드들과 set메소드를 통해
+        //대분류에 해당하는 세부분야를 만들고, 원하는 Data를 리턴해준다.
+        private String bigId;
+        private String bigname;
+        private String[] middleList;
+        private String[] middleidList;
+        private String middleStr = "";
+        private String middleidStr = "";
 
-        public BigMiddleConnect(String bigname,int bigId){
+        public BigMiddleConnect(String bigname,String bigId){
+            //인자로 받는 bigname과 bigId를 통해 생성
             this.bigname = bigname;
             this.bigId = bigId;
         }
 
         public void setMiddleList() {
+            //현재 객체의 bigId를 통해서 현재 객체의 대분류에 해당하는 세부분야의 쿼리문을 작성하고
+            //스트링 리스트로 저장
             String inserviceUrl = "http://ibtk.kr/inspectionAgencyDetail_api/";
             String inserviceKey = "3f7f0c56c14ad73f3a0534ba2999f4a2?";
-            String query = "model_query_pageable.enable=true&model_query_distinct=middlename&model_query={\"bigid\":\"0" + this.bigId + "\"}";
+            String query = "model_query_pageable.enable=true&model_query_distinct=middlename&model_query={\"bigid\":\"" + this.bigId + "\"}";
             String instrUrl = inserviceUrl + inserviceKey + query;
 
             try {
@@ -276,18 +294,25 @@ public class CheckActivity extends ActionBarActivity {
                 for (int j = 0; j < injArr.length(); j++) {
                     injson = injArr.getJSONObject(j);
                     String middleName = injson.getString("middlename");
+                    String middleId = injson.getString("middleid");
                     //System.out.println(middleName);
                     if(middleName.length() ==0)
                         continue;
                     middleStr = middleStr + middleName + ",";
+                    middleidStr = middleidStr + middleId + ",";
                 }
-
-                StringTokenizer st = new StringTokenizer(middleStr,",");
-                middleList = new String[st.countTokens()];
+                //쿼리문에서 얻어온 데이터에서 세부분야의 이름이 빈칸으로 되어있는 부분이 있기 떄문에
+                //해당 부분을 삭제하기 위해 StringTokenizer를 사용한다.
+                StringTokenizer middleNameSt = new StringTokenizer(middleStr,",");
+                StringTokenizer middleIdSt = new StringTokenizer(middleidStr,",");
+                middleList = new String[middleNameSt.countTokens()];
+                middleidList = new String[middleIdSt.countTokens()];
                 int i = 0;
-                while(st.hasMoreTokens()){
-                    String tmp = st.nextToken();
-                    middleList[i++] = new String(tmp);
+                while(middleNameSt.hasMoreTokens()){
+                    String tmpName = middleNameSt.nextToken();
+                    String tmpId = middleIdSt.nextToken();
+                    middleidList[i] = new String(tmpId);
+                    middleList[i++] = new String(tmpName);
                 }
             } catch (Exception e){
                 return;
@@ -298,41 +323,33 @@ public class CheckActivity extends ActionBarActivity {
         public String getBigname(){
             return bigname;
         }
-
+        public String getMiddleId(int id) { return middleidList[id];}
         public String[] getMiddleList(){
             return middleList;
         }
-        public String getMiddleName(int id){
-            return middleList[id];
-        }
-        public String getMiddleListTostring(){
-            String data = "";
-            for(int i=0;i<middleList.length;i++)
-                data += middleList[i];
-            return data;
-        }
 
-        public int getBigId(){
+        public String getBigId(){
             return bigId;
         }
     }
 
-    private class resultData {
-        String companyName;
-        String addr;
-        String range;
+    private class ResultData {
+        //검색을 통해 얻어진 데이터의 정보를 저장하고 있을 클래스
+        private String companyName;
+        private String addr;
+        private String accreditNumber;
 
-        public resultData(String companyName,String addr,String range){
+        public ResultData(String companyName,String addr,String accreditNumber){
             this.companyName = companyName;
             this.addr = addr;
-            this.range = range;
+            this.accreditNumber = accreditNumber;
         }
 
         public String getCompanyName(){
             return companyName;
         }
         public String getData(){
-            return companyName + "\n" + range + "\n";
+            return accreditNumber + "\n" + companyName + "\n" + addr +"\n";
         }
         public String getAddr(){
             return addr;
@@ -356,7 +373,7 @@ public class CheckActivity extends ActionBarActivity {
                 //JSONArray jARR로 부터 bigname과 bigid에 해당하는 data를 읽어옴
                 json = jArr.getJSONObject(i);
                 String bigName = json.getString("bigname");
-                int bigId = Integer.parseInt(json.getString("bigid"));
+                String bigId = json.getString("bigid");
                 bigList[i] = new String(bigName);
                 System.out.println(bigList[i]);
                 dataList[i] = new BigMiddleConnect(bigName,bigId);
@@ -391,11 +408,13 @@ public class CheckActivity extends ActionBarActivity {
     }
 
     public void makeSearchQuery(){
+        //검색버튼 클릭시에 작동하는 메소드로 선택된 대분류Id(selectBigid)와 세부분야Id(selectMiddleid)를 통해서
+        //API로 부터 해당하는 기관을 찾아준다.
         String searchUrl = "http://ibtk.kr/inspectionAgencyDetail_api/";
         String searchUrlKey = "3f7f0c56c14ad73f3a0534ba2999f4a2?";
-        String searchquery = "model_query_pageable={enable:true,pageSize:1000}&model_query_distinct=companyno";//&model_query={\"bigname\":\"" + selectBigname + "\"},{\"middlename\":\"" + selectMiddlename + "\"}]}";
+        String searchquery = "model_query={$and:[{\"bigid\":\"" + selectBigid + "\"},{\"middleid\":\"" + selectMiddleid + "\"}]}&model_query_pageable={enable:true,pageSize:100,sortOrders:[{property:\"accreditnumber\",direction:1}]}&model_query_distinct=companyno";
         String instrUrl = searchUrl + searchUrlKey + searchquery;
-        results = new Vector<resultData>();
+        results = new ArrayList<ResultData>();
 
         try {
             String inline = getDataFromUrl(instrUrl);
@@ -404,20 +423,15 @@ public class CheckActivity extends ActionBarActivity {
             JSONArray injArr = injson.getJSONArray("content");
 //            results = new resultData[injArr.length()];
             for (int j = 0; j < injArr.length(); j++) {
+                //얻어온 데이터의 회사명, 인증번호, 주소를 얻어와서 ResultData객체를 생성하고
+                //생성된 ResultData객체를 results벡터에 저장
                 injson = injArr.getJSONObject(j);
-                if(!(selectBigname.equals(injson.getString("bigname")))||!(selectMiddlename.equals(injson.getString("middlename")))) {
-                    continue;
-                }
-                else{
-                    String companyName = injson.getString("company");
-                    //System.out.println(middleName);
-                    String addr = injson.getString("corporateaddr");
-                    String range = injson.getString("rangepower");
-                    System.out.println(companyName + addr + range);
-                    results.addElement(new resultData(companyName, addr, range));
-                    System.out.println(results.size());
-
-                }
+                String companyName = injson.getString("company");
+                //System.out.println(middleName);
+                String addr = injson.getString("corporateaddr");
+                String accreditNumber = injson.getString("accreditnumber");
+//                System.out.println(companyName + addr + accreditNumber);
+                results.add(new ResultData(companyName, addr, accreditNumber));
 
             }
         } catch (Exception e){
@@ -428,6 +442,7 @@ public class CheckActivity extends ActionBarActivity {
     }
 
     public ArrayAdapter setBigAdapter() {
+        //bigList를 통해서 어레이 어댑터를 생성하고 리턴해줌
         ArrayAdapter tmpAdapter = new myAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, bigList);
         tmpAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
